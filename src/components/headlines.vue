13f52f60de1bef67243cb585b072ed24e8ba0cb2
x2h1z <zckuna@gmail.com>
2022-01-24 09:26:28 +0800

    first commit

---
+++ src/components/headlines.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="content">
+    <h1> {{ msg }} </h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "headlines",
+  mounted() {
+    this.msg = "1231"
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/tail.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="main">
+    <h1>hello world</h1>
+    <h2>{{ info }}</h2>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "home",
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => (info = response ))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
